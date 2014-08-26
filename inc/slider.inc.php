25cb7a8c20952ccb2091baa3c140e8b10ab61b5c
bowdi <bowdi912@gmail.com>
2014-08-26 17:59:11 +0200

    ok

--- inc/slider.inc.php
+++ inc/slider.inc.php
@@ -11,13 +11,16 @@
 							ORDER BY dateSlider DESC Limit 5"; 
 						$resSlider = mysql_query($sSlider) or die('Erreur SQL !'.$sSlider.'<br>'.mysql_error());
 					
-						while ($row_jx_slider = mysql_fetch_array($resSlider)) { 
+						if($row_jx_slider = mysql_fetch_array($resSlider)) { 
 
                         ?>
 						
                 <a href=""><img src="<?php echo $url; ?>img/slide/<?php echo $row_jx_slider['previewSlider']; ?>" alt="" title="<?php echo $row_jx_slider['titleSlider']; ?>" data-transition="slideInLeft"/></a>
          
+				<?php }else{ ?>
+				<img src="<?php echo $url; ?>img/slide/maq.jpg" alt="img_maquette" title="No pic to display" data-transition="none"/>
 				<?php } ?>
+				
             </div>
         </div>
 		</div>
